553409d702325f224113cd81f730579dccdb7569
Nicholas Molnar <nicholas@xmtp.com>
2023-06-24 19:34:02 -0400

    fix: show error

--- src/Runner.ts
+++ src/Runner.ts
@@ -1,26 +1,26 @@
-import createClient from './Client'
-import HandlerContext from './HandlerContext'
+import createClient from "./Client";
+import HandlerContext from "./HandlerContext";
 
-type Handler = (message: HandlerContext) => Promise<void>
+type Handler = (message: HandlerContext) => Promise<void>;
 
 export default async function run(handler: Handler) {
-  const client = await createClient()
+  const client = await createClient();
 
-  console.log(`Listening on ${client.address}`)
+  console.log(`Listening on ${client.address}`);
 
   for await (const message of await client.conversations.streamAllMessages()) {
-    console.log(`Got a message`, message)
+    console.log(`Got a message`, message);
 
     try {
       if (message.senderAddress == client.address) {
-        continue
+        continue;
       }
 
-      const context = new HandlerContext(message)
+      const context = new HandlerContext(message);
 
-      await handler(context)
+      await handler(context);
     } catch (e) {
-      console.log(`error`, message)
+      console.log(`error`, e, message);
     }
   }
 }
